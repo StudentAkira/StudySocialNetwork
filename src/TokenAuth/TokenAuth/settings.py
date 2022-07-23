2bc74010f45afad4cc8cf6153808a8b982ddcb78
Akira <am.1.00@bk.ru>
2022-07-23 14:30:24 +0300

    Tokens are connected

--- src/TokenAuth/TokenAuth/settings.py
+++ src/TokenAuth/TokenAuth/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'accounts',
 ]
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'TokenAuth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,6 +110,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 
 # Internationalization
--- src/TokenAuth/accounts/views.py
+++ src/TokenAuth/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class TestView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return render(request,
+                      template_name='test.html',
+                      context={'username': request.user.username})
