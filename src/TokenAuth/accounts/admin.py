34a781ca4e37ea3855532ffc1002b49f60c90567
Akira <am.1.00@bk.ru>
2022-07-31 21:07:43 +0300

    models for post system is ready

--- src/TokenAuth/accounts/admin.py
+++ src/TokenAuth/accounts/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 
 # Register your models here.
-from accounts.models import CustomUser, Profile
+from accounts.models import CustomUser, Profile, Post, PostImage
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    fields = [('username', 'password')]
+    fields = [('username', 'password', 'posts')]
 
 
 admin.site.unregister(Group)
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(PostImage)
--- src/TokenAuth/accounts/models.py
+++ src/TokenAuth/accounts/models.py
@@ -3,14 +3,27 @@ from .managers import CustomUserManager
 from django.db import models
 
 
+class PostImage(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    position = models.IntegerField(default=0)
+
+
 class Post(models.Model):
     article = models.CharField(default='', max_length=255)
     text = models.CharField(default='', max_length=4095)
+    images = models.ForeignKey(PostImage, on_delete=models.CASCADE, null=True)
+    likes = models.IntegerField(default=0)
+
+    def add_like(self):
+        self.likes += 1
+
+    def remove_like(self):
+        self.likes -= 1
 
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
-    posts = models.ForeignKey(Post, on_delete=models.PROTECT)
+    posts = models.ForeignKey(Post, on_delete=models.PROTECT, null=True)
 
 
 class Profile(models.Model):
